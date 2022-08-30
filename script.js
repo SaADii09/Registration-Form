5ffb6fa5ec8ec39ffed30060e4df250a1f4353a0
SaADii09 <111152857+SaADii09@users.noreply.github.com>
2022-08-31 02:03:47 +0500

    Update script.js

--- script.js
+++ script.js
@@ -2,14 +2,10 @@ let allchk = true;
 let a = 0;
 var uploaded_image ="";
 let imgchk = false;
-//  $('.date').datepicker({  
-//     format: 'dd-mm-yyyy'  
-//   });  
 
 const image_input = document.querySelector("#image-input");
 
 function print1() {
-    debugger
     
 const image_input = document.querySelector("#image-input");
     allchk = true;
@@ -21,8 +17,6 @@ const image_input = document.querySelector("#image-input");
     let pass2 = document.getElementById("pass1").value;
     let fname2 = document.getElementById("gname1").value;
     let dataCome = document.getElementById("dob1").value;
-    // let dob2 = dataCome.toLocaleDateString();
-    // console.log(dataCome);
     let cnumber2 = document.getElementById("cnumber1").value;
     let email2 = document.getElementById("email1").value;
     var genderSel1 = document.getElementsByName("genderSel");
@@ -37,12 +31,9 @@ const image_input = document.querySelector("#image-input");
     let ltrchk = /^(?=.*\d)(?=.*[a-zA-Z])([a-zA-Z0-9])+$/;
     let passchk = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,30}$/;
     let datecheck = /^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$/;
-    // let datchk = /^(0?[1-9]|1[012])[\/\-](0?[1-9]|[12][0-9]|3[01])[\/\-]\d{4}$/;
-    // let dat2chk= /^(3[01]|[1-2][0-9]|0?[1-9])[/](1[0-2]|0?[1-9])[/](([0-9]{2})?[0-9]{2})$/;
-    // let datecheck = /^(0[1-9]|1[0-2])[/](0[1-9]|[12][0-9]|3[01])[/]\d{4}$/;
 
 
-    console.log(allchk);
+  
     //province
     if (province2 == "") {
         allchk = false;
@@ -52,7 +43,7 @@ const image_input = document.querySelector("#image-input");
     }
 
 
-    console.log(allchk);
+
     //city
     if (city2 == "" || !txtchk.test(city2)) {
         allchk = false;
@@ -62,7 +53,7 @@ const image_input = document.querySelector("#image-input");
     }
 
 
-    console.log(allchk);
+ 
     //username
     if (uname2 == "" || !ltrchk.test(uname2)) {
         allchk = false;
@@ -73,7 +64,7 @@ const image_input = document.querySelector("#image-input");
     }
 
 
-    console.log(allchk);
+
     //password
     if (passchk == "" || !passchk.test(pass2)) {
         allchk = false;
@@ -85,7 +76,7 @@ const image_input = document.querySelector("#image-input");
     }
 
 
-    console.log(allchk);
+
     //email
     if (email2 == "" || !emailChk.test(email2)) {
         allchk = false;
@@ -97,7 +88,6 @@ const image_input = document.querySelector("#image-input");
     }
 
 
-    console.log(allchk);
     //contact number
     if (cnumber2 == "" || !pchk.test(cnumber2)) {
         allchk = false;
@@ -118,7 +108,7 @@ const image_input = document.querySelector("#image-input");
     }
 
 
-    console.log(allchk);
+
     //full name
     if (fname2 == "" || !txtchk.test(fname2)) {
         allchk = false;
@@ -128,7 +118,6 @@ const image_input = document.querySelector("#image-input");
     }
 
 
-    console.log(allchk);
     //Gender
     while (!gValid && i < genderSel1.length) {
         if (genderSel1[i].checked) {
@@ -145,37 +134,17 @@ const image_input = document.querySelector("#image-input");
         document.getElementById("nice").style.border = "#ced4da solid 2px";
     }
     
-    console.log(allchk);
     //checkbox
     if (!conditionSel1.checked) {
         allchk = false;
         alert("Accept terms & conditions.");
-        // document.getElementById("labelSel").style.textDecoration = "underline";
-        // document.getElementById("labelSel").style.color = "red";
     }
-    // else {
-
-    //     document.getElementById("labelSel").style.textDecoration = "none";
-    //     document.getElementById("labelSel").style.color = "black";
-     // }
-    // image_input.addEventListener("change", function() {
-    //     const reader = new FileReader();
-    //     reader.addEventListener("load", () => {
-    //       const uploaded_image = reader.result;
-    //       document.getElementById("display-image").style.backgroundImage = `url(${uploaded_image})`;
-    //     });
-    //     reader.readAsDataURL(this.files[0]);
-    //   });
     if(imgchk == false){
 document.getElementById("display-image").style.border = "red 1px solid";
     }else{
         
 document.getElementById("display-image").style.border = "black 1px solid";
     }
-
-
-    console.log(allchk);
-    // document.write(allchk);
     return allchk;
 }
 
@@ -194,86 +163,3 @@ image_input.addEventListener("change", function() {
     });
     reader.readAsDataURL(this.files[0]);
   });
-
-
-
-
-// /////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // if (!formValid) alert("Must check some option!");
-    // return formValid;
-
-// function validatedate(inputText)
-//   {
-//   var dateformat = /^(0?[1-9]|1[012])[\/\-](0?[1-9]|[12][0-9]|3[01])[\/\-]\d{4}$/;
-//   // Match the date format through regular expression
-//   if(inputText.value.match(dateformat))
-//   {
-//   document.form1.text1.focus();
-//   //Test which seperator is used '/' or '-'
-//   var opera1 = inputText.value.split('/');
-//   var opera2 = inputText.value.split('-');
-//   lopera1 = opera1.length;
-//   lopera2 = opera2.length;
-//   // Extract the string into month, date and year
-//   if (lopera1>1)
-//   {
-//   var pdate = inputText.value.split('/');
-//   }
-//   else if (lopera2>1)
-//   {
-//   var pdate = inputText.value.split('-');
-//   }
-//   var mm  = parseInt(pdate[0]);
-//   var dd = parseInt(pdate[1]);
-//   var yy = parseInt(pdate[2]);
-//   // Create list of days of a month [assume there is no leap year by default]
-//   var ListofDays = [31,28,31,30,31,30,31,31,30,31,30,31];
-//   if (mm==1 || mm>2)
-//   {
-//   if (dd>ListofDays[mm-1])
-//   {
-//   alert('Invalid date format!');
-//   return false;
-//   }
-//   }
-//   if (mm==2)
-//   {
-//   var lyear = false;
-//   if ( (!(yy % 4) && yy % 100) || !(yy % 400))
-//   {
-//   lyear = true;
-//   }
-//   if ((lyear==false) && (dd>=29))
-//   {
-//   alert('Invalid date format!');
-//   return false;
-//   }
-//   if ((lyear==true) && (dd>29))
-//   {
-//   alert('Invalid date format!');
-//   return false;
-//   }
-//   }
-//   }
-//   else
-//   {
-//   alert("Invalid date format!");
-//   document.form1.text1.focus();
-//   return false;
-//   }
-//   }
